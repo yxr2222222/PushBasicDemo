9e29284f9d866397ac6686ed430e2f6d6b6e55fb
ciba <>
2025-05-21 10:11:20 +0800

    已知问题优化

--- PushBasic/src/main/java/com/yxr/pushbasic/activity/MfrMessageActivity.java
+++ PushBasic/src/main/java/com/yxr/pushbasic/activity/MfrMessageActivity.java
@@ -1,44 +1,26 @@
 package com.yxr.pushbasic.activity;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-
-import com.umeng.message.UmengNotifyClick;
+import com.umeng.message.UmengNotifyClickActivity;
 import com.umeng.message.entity.UMessage;
 import com.yxr.pushbasic.PushManager;
 import com.yxr.pushbasic.config.PushConfig;
 
-public class MfrMessageActivity extends Activity {
+public class MfrMessageActivity extends UmengNotifyClickActivity {
 
-    private final UmengNotifyClick mNotificationClick = new UmengNotifyClick() {
-        @Override
-        public void onMessage(UMessage msg) {
-            PushConfig pushConfig = PushManager.getInstance().getPushConfig();
+    @Override
+    protected void onMessage(UMessage msg) {
+        PushConfig pushConfig = PushManager.getInstance().getPushConfig();
 
-            if (pushConfig != null && pushConfig.getOnNotifyClickCallback() != null) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        pushConfig.getOnNotifyClickCallback().onNotifyClick(msg);
-                        finish();
-                    }
-                });
-            } else {
-                finish();
-            }
+        if (pushConfig != null && pushConfig.getOnNotifyClickCallback() != null) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    pushConfig.getOnNotifyClickCallback().onNotifyClick(msg);
+                    finish();
+                }
+            });
+        } else {
+            finish();
         }
-    };
-
-    @Override
-    protected void onCreate(Bundle bundle) {
-        super.onCreate(bundle);
-        mNotificationClick.onCreate(this, getIntent());
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-        mNotificationClick.onNewIntent(intent);
     }
 }
