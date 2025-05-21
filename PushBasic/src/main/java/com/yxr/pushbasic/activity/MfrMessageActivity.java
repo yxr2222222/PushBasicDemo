bb917b33376923f57601edcf2d93ba0c5977adbb
ciba <>
2025-05-21 09:56:54 +0800

    已知问题优化

--- PushBasic/src/main/java/com/yxr/pushbasic/activity/MfrMessageActivity.java
+++ PushBasic/src/main/java/com/yxr/pushbasic/activity/MfrMessageActivity.java
@@ -9,21 +9,18 @@ import com.umeng.message.entity.UMessage;
 import com.yxr.pushbasic.PushManager;
 import com.yxr.pushbasic.config.PushConfig;
 
-import java.util.Map;
-
 public class MfrMessageActivity extends Activity {
 
     private final UmengNotifyClick mNotificationClick = new UmengNotifyClick() {
         @Override
         public void onMessage(UMessage msg) {
             PushConfig pushConfig = PushManager.getInstance().getPushConfig();
-            final Map<String, String> extra = msg == null ? null : msg.getExtra();
 
             if (pushConfig != null && pushConfig.getOnNotifyClickCallback() != null) {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        pushConfig.getOnNotifyClickCallback().onNotifyClick(extra);
+                        pushConfig.getOnNotifyClickCallback().onNotifyClick(msg);
                         finish();
                     }
                 });
--- PushBasic/src/main/java/com/yxr/pushbasic/callback/OnNotifyClickCallback.java
+++ PushBasic/src/main/java/com/yxr/pushbasic/callback/OnNotifyClickCallback.java
@@ -2,8 +2,10 @@ package com.yxr.pushbasic.callback;
 
 import androidx.annotation.Nullable;
 
+import com.umeng.message.entity.UMessage;
+
 import java.util.Map;
 
 public interface OnNotifyClickCallback {
-    void onNotifyClick(@Nullable Map<String, String> extra);
+    void onNotifyClick(@Nullable UMessage msg);
 }
